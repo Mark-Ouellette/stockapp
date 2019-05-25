a1aaec421c91e293e4052a41f98561bc38d25e4f
Mark Ouellette <ouel4390@mylaurier.ca>
2019-05-25 23:31:15 +0000

    stock search working with ticker lookup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @nothing = "Hey bitch you forgot to enter a symbol"
+    elsif
+      if params[:id]
+        begin
+          @stock = StockQuote::Stock.quote(params[:id])
+        rescue StandardError
+          @error = "that stock quote does not exist"
+        end
+      end
+    end
   end
+  
   
   def about
   end
